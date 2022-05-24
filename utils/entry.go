a3bc83cd4cc46380f64b84030bcb9d1722bca781
blde <570516599@qq.com>
2022-05-24 23:37:42 +0800

    feat: add entry file

---
+++ utils/entry.go
@@ -0,0 +1,167 @@
+package utils
+
+import (
+	"encoding/binary"
+	"time"
+)
+
+type ValueStruct struct {
+	Meta      byte
+	Value     []byte
+	ExpiresAt uint64
+
+	Version uint64 // This field is not serialized. Only for internal usage.
+}
+
+// 对value 和过期时间进行编码
+func (vs *ValueStruct) EncodedSize() uint32 {
+	sz := len(vs.Value) + 1
+	enc := sizeVarint(vs.ExpiresAt)
+	return uint32(sz + enc)
+}
+
+func (vs *ValueStruct) DecodeValue(buf []byte) {
+	vs.Meta = buf[0]
+	var sz int
+	//  binary.Uvarint(buf[1:]) 有疑问
+	vs.ExpiresAt, sz = binary.Uvarint(buf[1:])
+	vs.Value = buf[1+sz:]
+}
+
+func (vs *ValueStruct) encodeValue(b []byte) uint32 {
+	b[0] = vs.Meta
+	sz := binary.PutUvarint(b[1:], vs.ExpiresAt)
+	n := copy(b[1+sz:], vs.Value)
+	return uint32(1 + sz + n)
+}
+
+// sizeVarint 这个是计算过期实际占用几个字节
+func sizeVarint(x uint64) (n int) {
+	for {
+		n++
+		x >>= 7
+		if x == 0 {
+			break
+		}
+	}
+	return n
+}
+
+type Entry struct {
+	Key       []byte
+	Value     []byte
+	ExpiresAt uint64
+
+	Meta         byte
+	Version      uint64
+	Offset       uint32
+	Hlen         int // Length of the header.
+	ValThreshold int64
+}
+
+func NewEntry(key, value []byte) *Entry {
+	return &Entry{
+		Key:   key,
+		Value: value,
+	}
+}
+
+func (e *Entry) Entry() *Entry {
+	return e
+}
+
+
+func (e *Entry) IsDeletedOrExpired() bool {
+	// 如果value是nil 返回true
+	if e.Value == nil {
+		return true
+	}
+	// 过期时间等于0代表持久化
+	if e.ExpiresAt == 0 {
+		return false
+	}
+	// 和当前的时间戳做对比
+	return e.ExpiresAt <= uint64(time.Now().Unix())
+}
+
+// 查看当前的ttl
+func (e *Entry) WithTTL (dur time.Duration) *Entry {
+	e.ExpiresAt = uint64(time.Now().Add(dur).Unix())
+	return e
+}
+
+// ValueStruct的size
+func (e *Entry) EncodedSize() uint32 {
+	sz := len(e.Value)
+	enc := sizeVarint(uint64(e.Meta))
+	enc += sizeVarint(e.ExpiresAt)
+	return uint32(sz + enc)
+}
+
+func (e *Entry) EstimateSize(threshold int) int {
+	if len(e.Value) < threshold {
+		return len(e.Key) + len(e.Value) + 1// 1指meta
+	}
+	return len(e.Key) + 12 + 1
+}
+
+// 头结点
+type Header struct {
+	KLen      uint32
+	VLen      uint32
+	ExpiresAt uint64
+	Meta      byte
+}
+
+// +------+----------+------------+--------------+-----------+
+// | Meta | UserMeta | Key Length | Value Length | ExpiresAt |
+// +------+----------+------------+--------------+-----------+
+func (h Header) Encode(out []byte) int {
+	// 这里的Meta是一个字节
+	out[0] = h.Meta
+	index := 1
+	index += binary.PutUvarint(out[index:], uint64(h.KLen))
+	index += binary.PutUvarint(out[index:], uint64(h.VLen))
+	index += binary.PutUvarint(out[index:], h.ExpiresAt)
+	return index
+}
+
+// 从header对象字节块读取字节数
+func (h *Header) Decode(buf []byte) int {
+	h.Meta = buf[0]
+	index := 1 
+	klen, count := binary.Uvarint(buf[index:])
+	h.KLen = uint32(klen)
+	index += count
+	vlen, count := binary.Uvarint(buf[index:])
+	h.VLen = uint32(vlen)
+	index += count
+	h.ExpiresAt, count = binary.Uvarint(buf[index:])
+	return index + count
+}
+
+
+func (h *Header) DecodeFrom(reader *HashReader) (int, error) {
+	var err error
+	h.Meta, err = reader.ReadByte()
+	if err != nil {
+		return 0, err
+	}
+
+	// todo ?????
+	klen, err := binary.ReadUvarint(reader)
+	if err != nil {
+		return 0, err
+	}
+	h.KLen = uint32(klen)
+	vlen, err := binary.ReadUvarint(reader)
+	if err != nil {
+		return 0, err
+	}
+	h.VLen = uint32(vlen)
+	h.ExpiresAt, err = binary.ReadUvarint(reader)
+	if err != nil {
+		return 0, err
+	}
+	return reader.BytesRead, nil
+}
--- utils/skiplist.go
+++ utils/skiplist.go
@@ -133,6 +133,62 @@ func (s *Skiplist) getHead() *node {
 	return s.arena.getNode(s.headOffset)
 }
 
+
+func (s *Skiplist) findNear(key []byte, less bool, allowEqual bool) (*node, bool) {
+	x := s.getHead()
+	level := int(s.getHead() - 1)
+	for {
+		next := s.getNext(x, level)
+		if next == nil {
+			if level > 0 {
+				level--
+				continue
+			}
+			if !less {
+				return nil, false
+			}
+			if x == s.getHead() {
+				return nil, false
+			}
+			return x, false
+		}
+
+		nextKey := next.key(s.arena)
+		cmp := CompareKeys(key, nextKey)
+		if cmp > 0 {
+			x = next
+			continue
+		}
+		if cmp == 0 {
+			if allowEqual {
+				return next, true
+			}
+			if !less {
+				return s.getNext(next, 0), false
+			}
+			if level > 0 {
+				level--
+				continue
+			}
+			if x == s.getHead() {
+				return nil, false
+			}
+			return x, false
+		}
+		if level > 0 {
+			level--
+			continue
+		}
+		if !less {
+			return next, false
+		}
+		if x == s.getHead() {
+			return nil, false
+		}
+		return x, false
+	}
+}
+
 func (s *Skiplist) Add (e *Entry) {
 	key, v := e.Key, ValueStruct {
 		Meta: e.Meta,
@@ -148,7 +204,7 @@ func (s *Skiplist) Add (e *Entry) {
 	for i := int(listHeight) - 1; i >= 0; i-- {
 		prev[i], next[i] = s.findSpliceForLevel(key, prev[i + 1], i)
 		if prev[i] == next[i] {
-			vo : = s.arena.putVal(v)
+			vo := s.arena.putVal(v)
 			encValue := encodeValue(vo, v.EncodeSize())
 			prevNode := s.arena.getNode(prev[i])
 			prevNode.setValue(s.arena, encValue)
@@ -157,15 +213,269 @@ func (s *Skiplist) Add (e *Entry) {
 	}
 }
 
+func (s *Skiplist) findSpliceForLevel(key []byte, before uint32, level int) (uint32, uint32) {
+	for {
+		beforeNode := s.arena.getNode(before)
+		next := beforeNode.getNextOffset(level)
+		nextNode := s.arena.getNode(next)
+		if nextNode == nil {
+			return before, next
+		}
+		nextKey := nextNode.key(s.arena)
+		cmp := CompareKeys(key, nextKey)
+		if cmp == 0{
+			return next, next
+		}
+		if cmp < 0{
+			return before, next
+		}
+		before = next
+	}
+}
+
+func (s *Skiplist) getHeight() int32 {
+	return atomic.LoadInt32(&s.height)
+}
+
+func (s *Skiplist) Add(e *Entry) {
+	key, v := e.Key, ValueStruct{
+		Meta:      e.Meta,
+		Value:     e.Value,
+		ExpiresAt: e.ExpiresAt,
+		Version:   e.Version,
+	}
+	listHeight := s.getHeight()
+	var prev [maxHeight + 1]uint32
+	var next [maxHeight + 1]uint32
+	prev[listHeight] = s.headOffset
+	for i := int(listHeight) - 1; i >= 0; i-- {
+
+		prev[i], next[i] = s.findSpliceForLevel(key, prev[i+1], i)
+		if prev[i] == next[i] {
+			vo := s.arena.putVal(v)
+			encValue := encodeValue(vo, v.EncodedSize())
+			prevNode := s.arena.getNode(prev[i])
+			prevNode.setValue(s.arena, encValue)
+			return
+		}
+	}
+
+	height := s.randomHeight()
+	x := newNode(s.arena, key, v, height)
+
+	listHeight = s.getHeight()
+	for height > int(listHeight) {
+		if atomic.CompareAndSwapInt32(&s.height, listHeight, int32(height)) {
+
+			break
+		}
+		listHeight = s.getHeight()
+	}
+
+	for i := 0; i < height; i++ {
+		for {
+			if s.arena.getNode(prev[i]) == nil {
+				AssertTrue(i > 1) 
+				prev[i], next[i] = s.findSpliceForLevel(key, s.headOffset, i)
+
+				AssertTrue(prev[i] != next[i])
+			}
+			x.tower[i] = next[i]
+			pnode := s.arena.getNode(prev[i])
+			if pnode.casNextOffset(i, next[i], s.arena.getNodeOffset(x)) {
+				break
+			}
+
+			prev[i], next[i] = s.findSpliceForLevel(key, prev[i], i)
+			if prev[i] == next[i] {
+				AssertTruef(i == 0, "Equality can happen only on base level: %d", i)
+				vo := s.arena.putVal(v)
+				encValue := encodeValue(vo, v.EncodedSize())
+				prevNode := s.arena.getNode(prev[i])
+				prevNode.setValue(s.arena, encValue)
+				return
+			}
+		}
+	}
+}
 
 
+func (s *Skiplist) Empty() bool {
+	return s.findLast() == nil
+}
+
+func (s *Skiplist) findLast() *node {
+	n := s.getHead()
+	level := int(s.getHeight()) - 1
+	for {
+		next := s.getNext(n, level)
+		if next != nil {
+			n = next
+			continue
+		}
+		if level == 0 {
+			if n == s.getHead() {
+				return nil
+			}
+			return n
+		}
+		level--
+	}
+}
+
+func (s *Skiplist) Search (key []byte) ValueStruct {
+	n, _ := s.findNear(key, false, true)
+	if n == nil {
+		return ValueStruct{}
+	}
+
+	nextKey := s.arena.getKey(n.keyOffset, n.keySize)
+	if !SamKey(key, nextKey) {
+		return ValueStruct{}
+	}
+
+	valOffset, valSize := n.getValueOffset()
+	vs := s.arena.getVal(valOffset, valSize)
+	vs.ExpiresAt = ParseTs(nextKey)
+	return vs
+}
+
+func (s *Skiplist) NewSkipListIterator() Iterator {
+	s.IncrRef()
+	return &SkipListIterator{list: s}
+}
+
+func (s *Skiplist) MemSize() int64 { return s.arena.size() }
+
+func (s *Skiplist) Draw(align bool) {
+	reverseTree := make([][]string, s.getHeight())
+	head := s.getHead()
+	for level := int(s.getHeight()) - 1; level >= 0; level-- {
+		next := head
+		for {
+			var nodeStr string
+			next = s.getNext(next, level)
+			if next != nil {
+				key := next.key(s.arena)
+				vs := next.getVs((s.arena))
+				nodeStr = fmt.Sprintf("%s(%s)", key, vs.Value)
+			} else {
+				break
+			}
+			reverseTree[level] = append(reverseTree[level], nodeStr)
+		}
+	}
+
+	if align && s.getHeight() > 1 {
+		baseFloor := reverseTree[0]
+		for level := 1; level < int(s.getHeight()); level++ {
+			pos := 0
+			for _, ele := range baseFloor {
+				if pos ==len(reverseTree[level]) {
+					break
+				}
+				if ele != reverseTree[level][pos] {
+					newStr := fmt.Sprintf(strings.Repeat("_", len(ele)))
+					reverseTree[level] = append(reverseTree[level][:pos+1], reverseTree[level][pos:]...)
+					reverseTree[level][pos] = newStr
+				}
+				pos++
+			}
+		}
+	}
+
+	for level := int(s.getHeight()) - 1; level >= 0; level-- {
+		fmt.Printf("%d: ", level)
+		for pos, ele := range reverseTree[level] {
+			if pos == len(reverseTree[level])-1 {
+				fmt.Printf("%s  ", ele)
+			} else {
+				fmt.Printf("%s->", ele)
+			}
+		}
+		fmt.Println()
+	}
+}
 
 
+type SkipListIterator struct {
+	list *Skiplist
+	n    *node
+}
+
+func (s *SkipListIterator) Rewind() {
+	s.SeekToFirst()
+}
+
+func (s *SkipListIterator) Item() Item {
+	return &Entry{
+		Key:       s.Key(),
+		Value:     s.Value().Value,
+		ExpiresAt: s.Value().ExpiresAt,
+		Meta:      s.Value().Meta,
+		Version:   s.Value().Version,
+	}
+}
+
+func (s *SkipListIterator) Close() error {
+	s.list.DecrRef()
+	return nil
+}
+
+func (s *SkipListIterator) Valid() bool { return s.n != nil }
+
+func (s *SkipListIterator) Key() []byte {
+	return s.list.arena.getKey(s.n.keyOffset, s.n.keySize)
+	
+}
+
+func (s *SkipListIterator) Value() ValueStruct {
+	valOffset, valSize := s.n.getValueOffset()
+	return s.list.arena.getVal(valOffset, valSize)
+}
+
+func (s *SkipListIterator) ValueUint64() uint64 {
+	return s.n.value
+}
+
+func (s *SkipListIterator) Next() {
+	AssertTrue(s.Valid())
+	s.n = s.list.getNext(s.n, 0)
+}
+
+func (s *SkipListIterator) Prev() {
+	AssertTrue(s.Valid())
+	s.n, _ = s.list.findNear(s.Key(), true, false) // find <. No equality allowed.
+}
+
+func (s *SkipListIterator) Seek(target []byte) {
+	s.n, _ = s.list.findNear(target, false, true) // find >=.
+}
+
+func (s *SkipListIterator) SeekForPrev(target []byte) {
+	s.n, _ = s.list.findNear(target, true, true) // find <=.
+}
 
 
+func (s *SkipListIterator) SeekToFirst() {
+	s.n = s.list.getNext(s.list.getHead(), 0)
+}
 
 
+func (s *SkipListIterator) SeekToLast() {
+	s.n = s.list.findLast()
+}
 
-
+type UniIterator struct {
+	iter     *Iterator
+	reversed bool
+}
 //go:linkname FastRand runtime.fastrand
 func FastRand() uint32
+
+
+func AssertTruef(b bool, format string, args ...interface{}) {
+	if !b {
+		log.Fatalf("%+v", errors.Errorf(format, args...))
+	}
+}
